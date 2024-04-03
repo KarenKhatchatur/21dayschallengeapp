570c1aab141103e11914df0f1d3b7a6a7515b099
KarenKhatchatur <karenkhatchatur@gmail.com>
2024-04-03 12:10:05 +0200

    21dayschallengeapp

--- 21dayschallengeapp/21dayschallengeapp/Program.cs
+++ 21dayschallengeapp/21dayschallengeapp/Program.cs
@@ -1,18 +1,17 @@
 ﻿var name = "Ewa";
 var sex = "Woman";
 var age = 30;
-var sex2 = "Man";
-var age2 = 18;
 
 if (sex == "Woman" && age < 30)
 {
     Console.WriteLine("Kobieta poniżej 30 lat");
 }
+
 if (name == "Ewa" && age == 30)
 {
     Console.WriteLine("Ewa lat 30");
 }
-if (sex2 == "Man" && age2 < 18)
+if (sex == "Man" && age < 18)
 {
-    Console.WriteLine("Niepełnoletni Mężczyzna");
+    Console.WriteLine("Niepełnoletni Mężczyzna")
 }
